6fd69b5ba4bdc0a4778908f19ad4704ce03fb4e3
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-06 02:50:15 -0600

    Connect to html in email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Myself'
-email['to'] = 'dummy152146@gmail.com' # Dummy Email
+email['to'] = 'dummy152146@gmail.com'  # Dummy Email
 email['subject'] = 'Success Manifesto'
 
-email.set_content('test text')
+email.set_content(html.substitute({'name': 'Your Name'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
