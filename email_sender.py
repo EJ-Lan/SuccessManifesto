17ecaa216b8ec298a0cb47689fb4277e1fd7d6d2
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-06 02:14:00 -0600

    Test sending emails

---
+++ email_sender.py
@@ -0,0 +1,15 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Myself'
+email['to'] = 'dummy152146@gmail.com' # Dummy Email
+email['subject'] = 'Success Manifesto'
+
+email.set_content('test text')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('dummy152146@gmail.com', 'mypassword@123')
+    smtp.send_message(email)
